1299884708614d2e964ff8fe7929c19ecad3f2b5
Your Name <you@example.com>
2025-09-08 11:17:58 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -155,11 +155,21 @@ for(let i=1;i<=a;i++){
 
 
 /*4.11.Multiplication of five/
-let a=5;
-for(let i=1;i<=10;i++){
-console.log(a*i);
+let a = 5;
+for (let i = 1; i <= 10; i++) 
+{
+  console.log(a+" * "+ i +" = "+(a * i));
 }
-//output:5,10,15,20,25,30,35,40,45,50
+//output:5*1=5
+5*2=10
+5*3=15
+5*4=20
+5*5=25
+5*6=30
+5*7=35
+5*8=40
+5*9=45
+5*10=50
 */
 
 
